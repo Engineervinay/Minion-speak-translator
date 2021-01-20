6f8f1c20e9a0d41312f4ed18cb11eaba5623d0c8
engineervinay <vinay1645.vp@gmail.com>
2021-01-20 16:45:25 +0530

    Translation done using api

--- app.js
+++ app.js
@@ -2,10 +2,19 @@ var txtInput=document.querySelector("#txt-Input")
 var bTranslate=document.querySelector("button")
 var txtOutput=document.querySelector("#txt-Output")
 
+function errorHandler(error)
+{
+    console.log("you have error : ", error)
+    alert("try again after one hour!!!")
+}
 function clickEventListener()
 {
-    console.log(txtInput.value);
-    txtOutput.innerText=txtInput.value;
+    url="https://api.funtranslations.com/translate/minion.json?text=" + txtInput.value;
+    fetch(url)
+    .then(response=>response.json())
+    .then(json=>txtOutput.innerText=json.contents.translated)
+    .catch(errorHandler)
+    
 }
 
 bTranslate.addEventListener("click", clickEventListener);
